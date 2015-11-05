7a5688d6bfdc787b45dc970fc17511b4ad9bd32a
Spedroza <clickmyclique@yahoo.com>
2015-11-04 20:24:57 -0800

    Add fixing html layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Wunder"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+  
+  #alerts
+  def alert_class_for(flash_type)
+    {
+      :success => 'alert-success',
+      :error => 'alert-danger',
+      :alert => 'alert-warning',
+      :notice => 'alert-info'
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+  
 end
